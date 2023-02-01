4164955df1c303b979de4ffdbe4db31fe3b533d4
reshmanov21 <38521366+reshmanov21@users.noreply.github.com>
2023-02-02 00:58:45 +0530

    Change3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 5000;
+app = express(),
+port = process.env.PORT || 5000;
   
   
 const fs = require('fs');
@@ -18,6 +18,7 @@ app.get('/getUsers',(req,res) => {
 	//console.log(__dirname + "***" + path.resolve(__dirname, "db.json"));
 	fs.readFile(path.resolve(__dirname, "db.json"), 'utf8', function(err, data){
 		data = JSON.parse(data);
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
 		res.send(data);
 	});
 });
@@ -27,15 +28,68 @@ app.get('/getTopScores',(req,res) => {
 	//console.log(__dirname + "***" + path.resolve(__dirname, "db.json"));
 	fs.readFile(path.resolve(__dirname, "db.json"), 'utf8', function(err, data){
 		data = JSON.parse(data);
+		//for (var i = 0; i < data.results.length; i++) {
+		//	console.log("***");
+		//	console.log(data.results[i].score);
+		//}
+		json = data.results;
+		json.sort(function(a, b){
+			return b.score - a.score;
+		});
+		
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+		res.send(json);
+	});
+});
+
+
+
+app.get('/getMaleTopScores',(req,res) => {
+	//console.log(__dirname + "***" + path.resolve(__dirname, "db.json"));
+	fs.readFile(path.resolve(__dirname, "db.json"), 'utf8', function(err, data){
+		data = JSON.parse(data);
+		json = data.results;
+		json.sort(function(a, b){
+			return b.score - a.score;
+		});
+		
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+		res.send(json);
+	});
+});
+
+
+app.get('/getFemaleTopScores',(req,res) => {
+	//console.log(__dirname + "***" + path.resolve(__dirname, "db.json"));
+	fs.readFile(path.resolve(__dirname, "db.json"), 'utf8', function(err, data){
+		data = JSON.parse(data);
+		json = data.results;
+		json.sort(function(a, b){
+			return b.score - a.score;
+		});
+		
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+		res.send(json);
+	});
+});
+
+
+
+
+app.get('/getTopScores',(req,res) => {
+	//console.log(__dirname + "***" + path.resolve(__dirname, "db.json"));
+	fs.readFile(path.resolve(__dirname, "db.json"), 'utf8', function(err, data){
+		data = JSON.parse(data);
 		for (var i = 0; i < data.results.length; i++) {
 			console.log("***");
-			console.log(data.results[i].dob.age);
+			console.log(data.results[i].score);
 		}
 		json = data.results;
 		json.sort(function(a, b){
-			return a.dob.age - b.dob.age;
+			return b.score - a.score;
 		});
 		
+		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
 		res.send(json);
 	});
 });
--- js/getScoreBoard.js
+++ js/getScoreBoard.js
@@ -1,39 +1,115 @@
-const uri = 'https://randomuser.me/api/?results=5';
+const uri = 'https://score-board.onrender.com/getUsers';
 
 const initDetails = {
     method: 'get',
     headers: {
         "Content-Type": "application/json; charset=utf-8"
     },
-    mode: "cors"
+    mode: "no-cors"
 }
 
-function getData() {
+function getData2() {
 
     fetch( uri, initDetails )
-                .then( response =>
-                {
-                    console.log("&&&&&");
-                    console.log(response);
-                    if ( response.status !== 200 )
-                    {
-                        console.log( 'Looks like there was a problem. Status Code: ' +
-                            response.status );
-                        return;
-                    }
+        .then( response =>
+        {
+            console.log("&&&&&");
+            console.log(response);
+			
+            if ( response.status !== 200 )
+            {
+                console.log( 'Looks like there was a problem. Status Code: ' +
+                response.status );
+                return;
+            }
 
-                    console.log( response.headers.get( "Content-Type" ) );
-					document.getElementById('scoreboard').innerHTML=response.json();
-                    return response.json();
-                }
-                )
-                .then( myJson =>
-                {
-                    console.log( JSON.stringify( myJson ) );
-                } )
-                .catch( err =>
-                {
-                    console.log( 'Fetch Error :-S', err );
-                } );
+            console.log( response.headers.get( "Content-Type" ) );
+			document.getElementById('scoreboard').innerHTML=response.json();
+            return response.json();
+        })
+    .then( myJson =>
+    {
+        console.log( JSON.stringify( myJson ) );
+        } )
+    .catch( err =>
+    {
+            console.log( 'Fetch Error :-S', err );
+    });
 
+}
+
+async function getData1() {
+	
+	const response = await fetch(uri);
+	console.log("$$$");
+	console.log(response);
+	var data = await response.json();
+	console.log(data);
+}
+
+function getAllScorersData() {
+	$.ajax({
+		url: 'http://localhost:5000/getTopScores',
+		dataType: 'json',
+		type:'GET',
+		cors: true ,
+		success: function(data) {
+		console.log(data);
+		var rows = '';
+		for (var i = 0; i < 5; i++) {
+			rows += '<tr>';
+            rows += '<td>' + data[i].id + '</td>';
+			rows += '<td>' + data[i].name + '</td>';
+			rows += '<td>' + data[i].score + '</td>';
+			rows += '<td>' + data[i].country + '</td>';
+			rows += '</tr>';
+		}
+		document.getElementById('table').innerHTML += rows;
+	}
+});
+}
+
+function getMaleScorersData() {
+	$.ajax({
+		url: 'http://localhost:5000/getTopScores',
+		dataType: 'json',
+		type:'GET',
+		cors: true ,
+		success: function(data) {
+		console.log(data);
+		var rows = '';
+		for (var i = 0; i < data.length; i++) {
+			rows += '<tr>';
+            rows += '<td>' + data[i].id + '</td>';
+			rows += '<td>' + data[i].name + '</td>';
+			rows += '<td>' + data[i].score + '</td>';
+			rows += '<td>' + data[i].country + '</td>';
+			rows += '</tr>';
+		}
+		document.getElementById('table').innerHTML += rows;
+	}
+});
+}
+
+function getFemaleScorersData() {
+	$.ajax({
+		url: 'http://localhost:5000/getTopScores',
+		dataType: 'json',
+		type:'GET',
+		cors: true ,
+		success: function(data) {
+		console.log(data);
+		var rows = '';
+		for (var i = 0; i < data.length; i++) {
+			rows += '<tr>';
+            rows += '<td>' + data[i].id + '</td>';
+			rows += '<td>' + data[i].name + '</td>';
+			rows += '<td>' + data[i].score + '</td>';
+			rows += '<td>' + data[i].country + '</td>';
+			rows += '</tr>';
+		}
+		document.getElementById('table').innerHTML += rows;
+	}
+});
+	
 }
